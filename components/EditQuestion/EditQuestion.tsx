1c5030a837d903ef863b707820f70bfdc5718cd7
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-09-25 23:35:04 -0400

    experimenting with methods for adding elements in place

---
+++ components/EditQuestion/EditQuestion.tsx
@@ -0,0 +1,27 @@
+import { SurveyQuestion } from "@/pages/edit";
+import { useEffect, useState } from "react";
+
+const EditQuestion = ({
+  questionData,
+  update,
+}: {
+  questionData: SurveyQuestion;
+  update: Function;
+}) => {
+  const [workingData, setWorkingData] = useState(questionData);
+  useEffect(() => {
+    update(workingData);
+  }, [workingData]);
+  return (
+    <article>
+      <input
+        type="text"
+        value={questionData.text}
+        onChange={(e) => {
+          setWorkingData({ ...workingData, text: e.target.value });
+        }}
+      />
+    </article>
+  );
+};
+export default EditQuestion;
--- pages/edit.tsx
+++ pages/edit.tsx
@@ -1,13 +1,16 @@
-interface SurveyQuestion {
+import { useState } from "react";
+import EditQuestion from "@/components/EditQuestion/EditQuestion";
+
+export interface SurveyQuestion {
   type: string;
   text: string;
 }
 
-interface MCQuestion extends SurveyQuestion {
+export interface MCQuestion extends SurveyQuestion {
   options: Array<{ id: number; text: string }>;
 }
 
-type SurveyDataset = {
+export type SurveyDataset = {
   title: string;
   description: string;
   questions: Array<SurveyQuestion>;
@@ -15,12 +18,54 @@ type SurveyDataset = {
 
 const sampleData: SurveyDataset = {
   title: "My Sample Survey",
+  description: "This is the sample data for this component",
+  questions: [{ type: "mc", text: "this is the text for Q1" }],
 };
 
 const EditPage = () => {
+  const [surveyData, setSurveyData] = useState(sampleData);
+
+  const addQuestionAtIndex = (index: number) => {
+    const newQuestion = { type: "mc", text: "this is a new question" };
+    const tempQuestions = [
+      ...surveyData.questions.slice(0, index - 1),
+      newQuestion,
+      ...surveyData.questions.slice(index),
+    ];
+    console.log(tempQuestions);
+
+    setSurveyData({ ...surveyData, questions: tempQuestions });
+  };
+
+  const updateQuestion = (questionData: SurveyQuestion, index: number) => {
+    const newQuestions = [...surveyData.questions];
+    newQuestions[index] = questionData;
+    setSurveyData({ ...surveyData, questions: newQuestions });
+  };
+
   return (
     <div>
       <h1>Edit Survey</h1>
+      {surveyData.questions.map((questionData, index) => {
+        return (
+          <>
+            <button
+              onClick={() => {
+                addQuestionAtIndex(index);
+              }}
+            >
+              +
+            </button>
+            <EditQuestion
+              key={index}
+              questionData={questionData}
+              update={(newQuestionData: SurveyQuestion) => {
+                updateQuestion(newQuestionData, index);
+              }}
+            />{" "}
+          </>
+        );
+      })}
     </div>
   );
 };

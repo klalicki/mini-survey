ce0b50dfd0b1bb32e5df1782ca2178bd10b16102
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-10-09 18:43:54 -0400

    implements scaleviewer

--- components/Viewers/ScaleViewer/ScaleViewer.tsx
+++ components/Viewers/ScaleViewer/ScaleViewer.tsx
@@ -1,9 +1,46 @@
 import { SurveySection } from "@/types/SectionTypes";
+import { Quickreply } from "@mui/icons-material";
 
 export const ScaleViewer = ({
   questionData,
 }: {
   questionData: SurveySection;
 }) => {
-  return <>{JSON.stringify(questionData)}</>;
+  const numbersToRender = [];
+  for (
+    let i = questionData.ScaleOptions.startNumber;
+    i <= questionData.ScaleOptions.endNumber;
+    i++
+  ) {
+    numbersToRender.push(i);
+  }
+  return (
+    <div>
+      <h2>{questionData.title}</h2>
+      <div className="scale-wrapper">
+        <div className="scale-label-start">
+          {questionData.ScaleOptions.startLabel}
+        </div>
+        <div className="scale-radio">
+          {numbersToRender.map((item) => {
+            return (
+              <div className="scale-radio-item" key={item}>
+                <label htmlFor={`${questionData.staticID}-${item}`}>
+                  {item}
+                </label>
+                <input
+                  type="radio"
+                  name={questionData.staticID}
+                  id={`${questionData.staticID}-${item}`}
+                />
+              </div>
+            );
+          })}
+        </div>{" "}
+        <div className="scale-label-end">
+          {questionData.ScaleOptions.endLabel}
+        </div>
+      </div>
+    </div>
+  );
 };

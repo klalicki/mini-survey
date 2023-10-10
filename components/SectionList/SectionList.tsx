aec14616e6ebe99a176d5824f7cbcb30ed61c1b9
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-10-10 00:33:21 -0400

    tweaks mobile styling

--- components/SectionList/SectionList.tsx
+++ components/SectionList/SectionList.tsx
@@ -2,15 +2,15 @@ import { AddSections } from "../AddSections/AddSections";
 import SectionSort from "../SectionSort/SectionSort";
 const SectionList = () => {
   return (
-    <div>
-      <div className="eq-container">
+    <div className="app-wrapper">
+      <div className="eq-container centered">
         <div className="eq-welcome">
           <h1>Welcome to the question editor.</h1>
           <h2>this is a subhead on the welcome section.</h2>
         </div>
       </div>
       <SectionSort></SectionSort>
-      <div className="eq-container">
+      <div className="eq-container centered">
         <div className="eq-final">
           <h2>Add new section:</h2>
           <AddSections />

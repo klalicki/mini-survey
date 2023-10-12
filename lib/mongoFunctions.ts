654e28679cb4da21de5ad3c7507977be3fb800f1
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-10-11 20:25:05 -0400

    adds function to delete all entries older than 24 hours

--- lib/mongoFunctions.ts
+++ lib/mongoFunctions.ts
@@ -43,10 +43,29 @@ export async function updateSurvey(id: string, newData: SurveyDataset) {
 export async function cleanUp() {
   try {
     console.clear();
-    console.log("cleaning up entries with no entries");
-    const a = await coll.deleteMany({ sections: null });
-    console.log(a);
-    return a;
+    // console.log("cleaning up entries with no entries");
+    // const a = await coll.deleteMany({ sections: null });
+    // console.log(a);
+    // return a;
+
+    // console.log("removing entries with no timeLastEdited field");
+    // const a = await coll.deleteMany({ timeLastEdited: null });
+    // console.log(a);
+
+    console.log("removing all items with old dates");
+
+    const dateCutoff = new Date().setHours(new Date().getHours() - 24);
+    // const dateCutoff = Date.now() - 10000;
+    // Construct the query to find documents where 'timeLastEdited' is less than the current time
+    const query = {
+      timeLastEdited: { $lt: dateCutoff },
+    };
+
+    const result = await coll.deleteMany(query);
+
+    console.log(`Deleted ${result.deletedCount} documents.`);
+
+    return result.deletedCount;
   } catch (error) {
     console.log(error);
   }

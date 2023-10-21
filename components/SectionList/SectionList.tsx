7bb5038ef05384931217461c3c78bb65ec823262
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-10-21 19:24:16 -0400

    starting to refactor to use tailwind

--- components/SectionList/SectionList.tsx
+++ components/SectionList/SectionList.tsx
@@ -5,19 +5,21 @@ import { SectionListContext } from "@/contexts/SectionListContext";
 import { SaveControls } from "../SaveControls/SaveControls";
 const SectionList = () => {
   return (
-    <div className="app-wrapper">
-      <div className="eq-container centered">
-        <div className="eq-welcome">
-          <h1>Question Editor</h1>
-          <h2>build your survey</h2>
-          <SaveControls />
+    <div className=" min-h-screen flex flex-col justify-center items-center bg-slate-100">
+      <div className="bg-white p-4 flex flex-col gap-2 shadow-lg rounded-md">
+        <div className="">
+          <div className="eq-welcome">
+            <h1 className=" text-2xl">Question Editor</h1>
+            <h2 className="text-xl">build your survey</h2>
+            <SaveControls />
+          </div>
         </div>
-      </div>
-      <SectionSort></SectionSort>
-      <div className="eq-container centered">
-        <div className="eq-final">
-          <h2>Add new section:</h2>
-          <AddSections />
+        <SectionSort></SectionSort>
+        <div className="eq-container centered">
+          <div className="eq-final">
+            <h2>Add new section:</h2>
+            <AddSections />
+          </div>
         </div>
       </div>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,6 @@ import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-import { getDocCount } from "@/lib/mongoFunctions";
-
 export default function Home() {
   const router = useRouter();
   return (
@@ -14,22 +12,24 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="splash-page">
-        <h1>
-          Welcome to <span className="brand">mini-survey!</span>
-        </h1>
-        <p>
-          This project is a work in progress. At this point the editor view is
-          fully functional with 3 section types and a functioning backend.
-        </p>
-        <button
-          className="btn-cta"
-          onClick={() => {
-            router.push("/survey/new");
-          }}
-        >
-          New
-        </button>
+      <main className="flex flex-col items-center justify-center h-screen">
+        <div className="max-w-xl">
+          <h1 className="text-5xl">
+            Welcome to <span className="brand">mini-survey!</span>
+          </h1>
+          <p className="text-3xl">
+            This project is a work in progress. At this point the editor view is
+            fully functional with 3 section types and a functioning backend.
+          </p>
+          <button
+            className="text-3xl p-4 border-red-500 border-solid border-2"
+            onClick={() => {
+              router.push("/survey/new");
+            }}
+          >
+            New
+          </button>
+        </div>
       </main>
     </>
   );
